d85b29b74eb6cef8ba3ac05b808f79b778145f02
MohammadAmitHasan <bhamithasan@gmail.com>
2022-02-13 13:23:06 +0600

    no of product function added

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('I am in a separate file')
+function updateNoOfProduct(productType, increase) {
+    const noOfProductInput = document.getElementById('no-of-' + productType);
+    const noOfProducts = parseInt(noOfProductInput.value);
+    if (increase) {
+        noOfProductInput.value = noOfProducts + 1;
+    }
+    else {
+        if (noOfProducts > 0) {
+            noOfProductInput.value = noOfProducts - 1;
+        }
+    }
+}
+// Phone section
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateNoOfProduct('phone', true);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateNoOfProduct('phone', false);
+})
+// Case section
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateNoOfProduct('case', true);
+})
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateNoOfProduct('case', false);
+})
